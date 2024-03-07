c128ebc7fbd0f8a8dde0db344c6aedeb63f286f0
EUNSEMIN <114456579+EUNSEMIN@users.noreply.github.com>
2024-03-07 23:13:07 +0900

    fix: Fix the bug

--- Dungeon_Jumak/Assets/Scripts/PlayerMovement.cs
+++ Dungeon_Jumak/Assets/Scripts/PlayerMovement.cs
@@ -16,7 +16,7 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 moveVector; // 이동벡터
 
     private bool checkFood = false; // 음식 들고 있는지 확인
-    public GameObject hand;
+    public GameObject hand; // 플레이어 손 위치
     private Queue<GameObject> foodQueue = new Queue<GameObject>(); // 충돌한 food 오브젝트를 저장하는 Queue
 
 
@@ -40,38 +40,53 @@ public class PlayerMovement : MonoBehaviour
         playerRb.MovePosition(playerRb.position + moveVector);
     }
 
-    private void OnCollisionEnter2D(Collision2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
+        // 국밥 충돌 확인
         if (other.gameObject.CompareTag("Food"))
         {
             if (!checkFood)
             {
-                foodQueue.Enqueue(other.gameObject);
-                other.transform.parent = hand.transform;
+                foodQueue.Enqueue(other.gameObject);// 충돌한 국밥 queue 저장
+                other.transform.parent = hand.transform;// 플레이어 손 오브젝트 하위로 이동
                 other.transform.localPosition = Vector3.zero;
                 checkFood = true;
             }
         }
 
-        if (other.gameObject.CompareTag("Table"))
+        //테이블 좌측 충돌 확인
+        if (other.gameObject.CompareTag("Table_L"))
         {
             if (checkFood)
             {
                 GameObject food = foodQueue.Dequeue();
-                Transform secondChild = other.transform.GetChild(1);
+                Transform secondChild = other.transform;
 
-                if (secondChild.childCount > 0)
-                {
-                    // 다른 Food가 있으면 첫 번째 자식 오브젝트에 놓기
-                    Transform firstChild = other.transform.GetChild(0);
-                    food.transform.parent = firstChild;
-                }
-                else
+                if (secondChild.childCount == 0)//테이블에 음식 있는지 확인
                 {
                     food.transform.parent = secondChild;
                 }
 
-                // Food 오브젝트를 두 번째 자식 오브젝트의 위치에 고정
+                // Food 오브젝트를 테이블 좌측 위치에 고정
+                food.transform.localPosition = Vector3.zero;
+                checkFood = false;
+            }
+        }
+
+        //테이블 우측 충돌 확인
+        if (other.gameObject.CompareTag("Table_R"))
+        {
+            if (checkFood)
+            {
+                GameObject food = foodQueue.Dequeue();
+                Transform secondChild = other.transform;
+
+                if (secondChild.childCount == 0)//테이블에 음식 있는지 확인
+                {
+                    food.transform.parent = secondChild;
+                }
+
+                // Food 오브젝트를 테이블 우측 위치에 고정
                 food.transform.localPosition = Vector3.zero;
                 checkFood = false;
             }
