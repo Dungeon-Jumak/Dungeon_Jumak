4b70af7d949326b77b17b1dd1b52a3a9433979c8
nyyyyyr <127469346+YuuuuuRAE@users.noreply.github.com>
2024-03-12 23:44:01 +0900

    Fix Customer AI
    1. AI 대부분 수정
    2. Navmesh agent, Bake 수정
    
    / --- Customer System 이어서 수정 ---/

--- Dungeon_Jumak/Assets/Scripts/CustomerMovement.cs
+++ Dungeon_Jumak/Assets/Scripts/CustomerMovement.cs
@@ -5,10 +5,15 @@ using UnityEngine.AI;
 
 public class CustomerMovement : MonoBehaviour
 {
-    [SerializeField]
-    Transform seat;
+    public Transform tempseat;
 
     NavMeshAgent agent;
+
+    [SerializeField]
+    private bool allocateSeat = false; //손님 자리 할당 변수
+
+    [SerializeField]
+    private Transform SelectedSeat; //선택된 자리 변수
 
     private void Start()
     {
@@ -19,7 +24,30 @@ public class CustomerMovement : MonoBehaviour
 
     private void Update()
     {
-        agent.SetDestination(seat.position);
+        // --- 자리 할당이 안되었을 경우 --- //
+        /*if (!allocateSeat)
+        {
+            allocateSeat = true;
+            SelectSeat();
+        }*/
+        // --- 에이전트 SetDestination --- //
+        agent.SetDestination(tempseat.position);
     }
 
+    /*
+    // --- 자리 지정 함수 --- //
+    private void SelectSeat()
+    {
+        for (int i = 0; i < data.seats.Length; i++)
+        {
+            if (!data.isAllocated[i])
+            {
+                SelectedSeat = data.seats[i];
+                data.isAllocated[i] = true;
+            }
+        }
+        // --- 에이전트 SetDestination --- //
+        agent.SetDestination(SelectedSeat.position);
+    }
+    */
 }
---
+++ Dungeon_Jumak/Assets/Scripts/CustomerSystem.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Search;
+using UnityEngine;
+
+public class CustomerSystem : MonoBehaviour
+{
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+}
