c85ecb6be4e9dbf77f5319f7afb97a11c900e9c2
EUNSEMIN <114456579+EUNSEMIN@users.noreply.github.com>
2024-03-16 16:46:45 +0900

    fix: Fix the convention

--- Dungeon_Jumak/Assets/Scripts/Player/PlayerMovement.cs
+++ Dungeon_Jumak/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,41 +1,30 @@
-using System;
-using System.Collections.Generic;
-using System.Collections.Specialized;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField]
-    private FloatingJoystick joystick;
-
-    [SerializeField]
-    private float moveSpeed; // 이동속도
+    [SerializeField] private FloatingJoystick joystick;
+    [SerializeField] private float moveSpeed; // 이동속도
 
     private Rigidbody2D playerRb;
-
     private Vector2 moveVector; // 이동벡터
-
-    [SerializeField]
-    private bool checkFood = false; // 음식 들고 있는지 확인
-
+    [SerializeField] private bool isCarryingFood = false; // 음식을 들고 있는지 확인
     public GameObject hand; // 플레이어 손 위치
 
-    private Queue<GameObject> foodQueue = new Queue<GameObject>(); // 충돌한 food 오브젝트를 저장하는 Queue
-
+    private Queue<GameObject> foodQueue = new Queue<GameObject>(); // 충돌한 Food 오브젝트를 저장하는 Queue
     public CookGukbap cookGukbap; // 국밥 카운트 참조 스크립트
 
     private void Awake()
     {
         playerRb = GetComponent<Rigidbody2D>();
-
     }
 
     private void Update()
     {
-        Move();
+        MovePlayer();
     }
 
-    private void Move()
+    private void MovePlayer()
     {
         moveVector = Vector2.zero;
         moveVector.x = joystick.Horizontal * moveSpeed * Time.deltaTime;
@@ -47,41 +36,39 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        // 국밥 충돌 확인
+        // 음식 충돌 확인
         if (other.gameObject.CompareTag("Food"))
         {
             FoodScript foodScript = other.GetComponent<FoodScript>();
 
-            if (!checkFood && foodScript.IsOnTable == false)
+            if (!isCarryingFood && !foodScript.IsOnTable)
             {
-                foodQueue.Enqueue(other.gameObject);// 충돌한 국밥 queue 저장
-                other.transform.parent = hand.transform;// 플레이어 손 오브젝트 하위로 이동
+                foodQueue.Enqueue(other.gameObject); // 충돌한 음식을 Queue에 저장
+                other.transform.parent = hand.transform; // 플레이어 손 아래로 이동
                 other.transform.localPosition = Vector3.zero;
-                checkFood = true;
+                isCarryingFood = true;
 
                 cookGukbap.gukbapCount--;
             }
         }
     }
 
-
-
     private void OnCollisionEnter2D(Collision2D other)
     {
-        //테이블 좌측 충돌 확인
+        // 테이블 충돌 확인
         if (other.gameObject.CompareTag("Table_L") || other.gameObject.CompareTag("Table_R"))
         {
-            Transform secondChild = other.transform.GetChild(0);
+            Transform tableChild = other.transform.GetChild(0);
 
-            if (checkFood && secondChild.childCount == 0)
+            if (isCarryingFood && tableChild.childCount == 0)
             {
-                checkFood = false;
+                isCarryingFood = false;
                 GameObject food = foodQueue.Dequeue();
                 FoodScript foodScript = food.GetComponent<FoodScript>();
 
-                food.transform.parent = secondChild;
+                food.transform.parent = tableChild;
 
-                // Food 오브젝트를 테이블 위치에 고정
+                // 음식을 테이블 위치에 고정
                 food.transform.localPosition = Vector3.zero;
 
                 // 상태를 업데이트하여 다음 충돌을 방지
