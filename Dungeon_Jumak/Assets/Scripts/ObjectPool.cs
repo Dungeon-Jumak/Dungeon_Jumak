b9f253b55a6a1b751f2f53c018939b95250af7d2
nyyyyyr <127469346+YuuuuuRAE@users.noreply.github.com>
2024-03-18 00:13:38 +0900

    [ADD] ObjectPool 스크립트 추가 (-유래)
    [ADD]
    UnityProject/Assets/Scripts/ObjectPool.cs추가
    
    ObjectPool.cs : 손님 프리팹을 오브젝트 풀링으로 관리하기 위한 스크립트 (-유래)

---
+++ Dungeon_Jumak/Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,62 @@
+using Newtonsoft.Json.Bson;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    public static ObjectPool Instance;
+
+    [SerializeField]
+    private GameObject poolingCustomerPrefab;
+
+    Queue<CustomerMovement> poolingObjectQueue = new Queue<CustomerMovement>();
+
+    private void Awake()
+    {
+        Instance = this;
+
+        Initialize(10);
+    }
+    
+    private void Initialize(int intCount)
+    {
+        for (int i = 0; i < intCount; i++)
+        {
+            poolingObjectQueue.Enqueue(CreateNewObject());
+        }
+    }
+
+    private CustomerMovement CreateNewObject()
+    {
+        var newObj = Instantiate(poolingCustomerPrefab).GetComponent<CustomerMovement>();
+        newObj.gameObject.SetActive(false);
+        newObj.transform.SetParent(transform);
+        return newObj;
+    }
+
+    public static CustomerMovement GetObject()
+    {
+        if(Instance.poolingObjectQueue.Count > 0)
+        {
+            var obj = Instance.poolingObjectQueue.Dequeue();
+            obj.transform.SetParent(null);
+            obj.gameObject.SetActive(true);
+            return obj;
+        }
+        else
+        {
+            var newObj = Instance.CreateNewObject();
+            newObj.gameObject.SetActive(true);
+            newObj.transform.SetParent(null);
+            return newObj;
+        }
+    }
+
+    public static void ReturnObject(CustomerMovement obj)
+    {
+        obj.gameObject.SetActive(false);
+        obj.transform.SetParent(Instance.transform);
+        Instance.poolingObjectQueue.Enqueue(obj);
+    }
+}
