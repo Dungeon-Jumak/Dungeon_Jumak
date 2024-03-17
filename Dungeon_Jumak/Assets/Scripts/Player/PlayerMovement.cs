cac42a3fb922c202b0d1588f835297542531ce5b
EUNSEMIN <114456579+EUNSEMIN@users.noreply.github.com>
2024-03-17 12:02:53 +0900

    add: Add animation

--- Dungeon_Jumak/Assets/Scripts/Player/PlayerMovement.cs
+++ Dungeon_Jumak/Assets/Scripts/Player/PlayerMovement.cs
@@ -14,9 +14,14 @@ public class PlayerMovement : MonoBehaviour
     private Queue<GameObject> foodQueue = new Queue<GameObject>(); // 충돌한 Food 오브젝트를 저장하는 Queue
     public CookGukbap cookGukbap; // 국밥 카운트 참조 스크립트
 
+    private Animator animator;
+    private SpriteRenderer spriter;
+
     private void Awake()
     {
         playerRb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriter = GetComponent<SpriteRenderer>();
     }
 
     private void Update()
@@ -30,6 +35,33 @@ public class PlayerMovement : MonoBehaviour
         moveVector.x = joystick.Horizontal * moveSpeed * Time.deltaTime;
         moveVector.y = joystick.Vertical * moveSpeed * Time.deltaTime;
 
+        // 조이스틱 입력을 기반으로 플레이어의 이동 방향을 설정
+        if (moveVector != Vector2.zero)
+        {
+            // 조이스틱 입력에 따라 플레이어의 애니메이션을 설정
+            if (moveVector.x > 0)
+            {
+                // 오른쪽 방향으로 이동
+                // 애니메이터에 오른쪽 방향을 나타내는 파라미터 값을 설정
+                animator.SetFloat("Horizontal", moveVector.x);
+                animator.SetFloat("isWalk", 1f);
+                spriter.flipX = true;
+            }
+            else if (moveVector.x < 0)
+            {
+                // 왼쪽 방향으로 이동
+                // 애니메이터에 왼쪽 방향을 나타내는 파라미터 값을 설정
+                animator.SetFloat("Horizontal", moveVector.x);
+                animator.SetFloat("isWalk", 1f);
+                spriter.flipX = false;
+
+            }
+        }
+        else
+        {
+            // 조이스틱 입력이 없을 때, 정지 상태로 애니메이션을 설정
+            animator.SetFloat("isWalk", -1f);
+        }
         // Rigidbody2D를 사용하여 위치를 업데이트
         playerRb.MovePosition(playerRb.position + moveVector);
     }
