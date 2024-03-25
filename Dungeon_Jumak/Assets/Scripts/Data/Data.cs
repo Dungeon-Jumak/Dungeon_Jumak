36c6098710ee69ce440fa51e4da290d8b0e37bfe
nyyyyyr <127469346+YuuuuuRAE@users.noreply.github.com>
2024-03-25 17:30:30 +0900

    [ADD] Data변수 추가

--- Dungeon_Jumak/Assets/Scripts/Data/Data.cs
+++ Dungeon_Jumak/Assets/Scripts/Data/Data.cs
@@ -9,11 +9,24 @@ public class Data
     //데이터에 있는 값을 다른 스크립트를 사용하기 위해서는 싱글톤으로서 사용하면 됨
     //ex) Data data = DataManager.Instance.data; => 이를 통해 Data.cs에 있는 변수값을 사용할 수 있음
 
+    public int curPlayerLV;
+    public int maxPlayerLV;
+
+    //---설정 관련---//
+    public bool isPlayBGM = false;
+    public bool isPause = false;
+
     //---CustomerSystem---//
     public int maxSeatSize = 2;
     public int curSeatSize = 0;
     public bool[] isAllocated = new bool[12];
     public bool[] isCustomer = new bool[12]; // 고객 테이블에 도착했는지 체크하기 위한 변수
+    public bool[] onTables = new bool[12]; //테이블 위에 음식을 체크하기 위한 변수
+    public bool[] isFinEat = new bool[12]; //다 먹었음을 알리는 변수
+    public int[] menuNums = new int[12];
+    public int curCoin = 0;
+    public int maxCoin = 999999;
+    public int[] foodIngredients; //음식 재료
 
     //---자리 해금 레벨---//
     public int curUnlockLevel = 1;
@@ -23,21 +36,9 @@ public class Data
     public int curMenuUnlockLevel = 1;
     public int maxMenuUnlockLevel = 3;
 
-    //---테이블 배열---//
-    public bool[] onTables = new bool[12]; //테이블 위에 음식을 체크하기 위한 변수
-    public bool[] isFinEat = new bool[12]; //다 먹었음을 알리는 변수
-
-    //---메뉴 관련---//
-    public int[] menuNums = new int[12];
-
-    //---사운드 관련---//
-    public bool isPlayBGM = false;
-
     //---불 미니게임 관련---//
     public float fireSize = 100f;
 
-    //---게임 정지 관련---//
-    public bool isPause = false;
-
+    //---던전 관련---//
 
 }
--- Dungeon_Jumak/Assets/Scripts/Data/DataManager.cs
+++ Dungeon_Jumak/Assets/Scripts/Data/DataManager.cs
@@ -31,7 +31,7 @@ public class DataManager : MonoBehaviour
     }
 
     // --- 게임 데이터 파일이름 설정 ("원하는 이름.json) --- //
-    string GameDataFileName = "GameData.json";
+    string gameDataFileName = "GameData.json";
 
     // --- 저장용 클래스 변수 --- //
     public Data data = new Data();
@@ -39,7 +39,7 @@ public class DataManager : MonoBehaviour
     // --- 데이터 로드 --- //
     public void LoadGameData()
     {
-        string filePath = Application.persistentDataPath + "/" + GameDataFileName;
+        string filePath = Application.persistentDataPath + "/" + gameDataFileName;
 
         // 저장된 게임이 이미 있을 경우
         if (File.Exists(filePath))
@@ -54,7 +54,7 @@ public class DataManager : MonoBehaviour
     {
         // 클래스 -> Json (true = 가독성 좋게 작성)
         string ToJsonData = JsonUtility.ToJson(data, true);
-        string filePath = Application.persistentDataPath + "/" + GameDataFileName;
+        string filePath = Application.persistentDataPath + "/" + gameDataFileName;
 
         //이미 저장된 파일 있다면 덮어쓰고, 없다면 새로 만들어서 저장
         File.WriteAllText(filePath, ToJsonData);
