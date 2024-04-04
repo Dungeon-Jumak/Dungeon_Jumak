7bbef08a870d7c15e83f8c1f8e9e1d347379f333
nyyyyyr <127469346+YuuuuuRAE@users.noreply.github.com>
2024-04-04 20:18:28 +0900

    [CHORE] BGM 매니저 소스 끊김 수정
    [CHORE]
    BGMManager 소스 끊김 수정

--- Dungeon_Jumak/Assets/Scripts/Scene/JumakScene.cs
+++ Dungeon_Jumak/Assets/Scripts/Scene/JumakScene.cs
@@ -5,6 +5,34 @@ using TMPro;
 
 public class JumakScene : BaseScene
 {
+    [SerializeField]
+    private BGMManager bgmManager;
+    [SerializeField]
+    private int bgmSoundTrack;
+    [SerializeField]
+    private float maxVolume;
+
+    [SerializeField]
+    private Data data;
+
+    private bool playBGM = false;
+
+    private void Start()
+    {
+        playBGM = false;
+
+        data = DataManager.Instance.data;
+        bgmManager = FindObjectOfType<BGMManager>();
+
+        //---BGM 사운드 트랙 설정---//
+        bgmSoundTrack = 0;
+        maxVolume = 0.3f;
+
+        //---기본 BGM 실행---//
+        bgmManager.Play(bgmSoundTrack);
+        bgmManager.FadeInMusic(maxVolume);
+        bgmManager.SetLoop();
+    }
     protected override void Init()
     {
         base.Init();
@@ -20,6 +48,32 @@ public class JumakScene : BaseScene
     {
         UpdateCoin();
         UpdateLevel();
+
+        if (!data.isPlayBGM)
+        {
+            playBGM = false;
+
+            bgmManager.CancelLoop();
+            bgmManager.Stop();
+        }
+
+        if (!playBGM && data.isPlayBGM)
+        {
+            playBGM = true;
+
+            bgmManager.Play(bgmSoundTrack);
+            bgmManager.FadeInMusic(maxVolume);
+            bgmManager.SetLoop();
+        }
+    }
+    public void BGMON()
+    {
+        data.isPlayBGM = true;
+    }
+
+    public void BGMOFF()
+    {
+        data.isPlayBGM = false;
     }
 
     public override void Clear()
