5c5bfb12b793ea18f196b0ca9c11ebbfd3d4b5f0
nyyyyyr <127469346+YuuuuuRAE@users.noreply.github.com>
2024-04-04 17:52:09 +0900

    [ADD] WaitingScene추가 (-유래)
    [ADD]
    게임 대기화면 WaitingScene 추가

---
+++ Dungeon_Jumak/Assets/Scripts/Scene/WaitingScene.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitingScene : MonoBehaviour
+{
+    [SerializeField]
+    private BGMManager bgmManager;
+    [SerializeField]
+    private int bgmSoundTrack;
+    [SerializeField]
+    private float maxVolume;
+
+    [SerializeField]
+    private Data data;
+
+    private bool playBGM = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playBGM = false;
+
+        data = DataManager.Instance.data;
+        bgmManager = FindObjectOfType<BGMManager>();
+
+        //---BGM 사운드 트랙 설정---//
+        bgmSoundTrack = 1;
+
+        //---기본 BGM 실행---//
+        bgmManager.Play(bgmSoundTrack);
+        bgmManager.FadeInMusic(maxVolume);
+        bgmManager.SetLoop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!data.isPlayBGM)
+        {
+            playBGM = false;
+
+            bgmManager.CancelLoop();
+            bgmManager.Stop();
+        }
+        else if (!playBGM && data.isPlayBGM)
+        {
+            playBGM = true;
+
+            bgmManager.Play(bgmSoundTrack);
+            bgmManager.FadeInMusic(maxVolume);
+            bgmManager.SetLoop();
+        }
+
+    }
+
+}
