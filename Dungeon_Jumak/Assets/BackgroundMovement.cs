f6cb6ae8f720ffa649ec7aef3f909fe94e3f6b93
EUNSEMIN <dmstpals104@ajou.ac.kr>
2024-04-07 23:10:46 +0900

    fix:Fix the dungeon scene

---
+++ Dungeon_Jumak/Assets/BackgroundMovement.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class BackgroundMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private int startIndex;
+
+    [SerializeField]
+    public int endIndex;
+
+    [SerializeField]
+    private Transform[] sprites;
+
+    //private float viewHeight;
+
+    /*private void Awake()
+    {
+        viewHeight = Camera.main.orthographicSize * 2;
+    }*/
+
+    void Update()
+    {
+        Vector3 curPos = transform.position;
+        Vector3 nextPos = Vector3.back * speed * Time.deltaTime;
+        transform.position = curPos + nextPos;
+
+        if (sprites[endIndex].position.z < -1)
+        {
+            Vector3 backSpritePos = sprites[startIndex].position;
+            Vector3 frontSpritePos = sprites[endIndex].position;
+            sprites[endIndex].transform.position = backSpritePos + Vector3.back * 4;
+
+            int startIndexSave = startIndex;
+            startIndex = endIndex;
+            endIndex = (startIndexSave-1 == -1) ? sprites.Length-1 : startIndexSave - 1;
+        }
+    }
+}
