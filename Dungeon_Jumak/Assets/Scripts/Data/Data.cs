4e401c4507257815a543d8d22c0fe471f4c1d4e7
DoRaGee_ <ruy0218@naver,com>
2024-04-11 16:23:40 +0900

    [Chore] 몬스터 HP data 추가

--- Dungeon_Jumak/Assets/Scripts/Data/Data.cs
+++ Dungeon_Jumak/Assets/Scripts/Data/Data.cs
@@ -51,5 +51,6 @@ public class Data
     public bool isObstacle = false;//Obstacle spawn 여부 확인
     public float runningTime = 0;//달리기 게임 실행 시간
     public int playerHP = 0;//HP - 하트 
+    public float monsterHP; // 몬스터HP - 하트 
 
 }
