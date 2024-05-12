b23baa82346512971c29bf1c73788026bac69608
EUNSEMIN <dmstpals104@ajou.ac.kr>
2024-05-12 20:46:10 +0900

    add:Add camera moving and test tilemap system in dunjeon scene

---
+++ Dungeon_Jumak/Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    [SerializeField] float smoothing = 0.2f;
+    private void FixedUpdate()
+    {
+        Vector3 targetPos = new Vector3(player.position.x, player.position.y, this.transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, targetPos, smoothing);
+    }
+}
